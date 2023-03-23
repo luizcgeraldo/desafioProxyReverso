13458dce081dd313a8f666e4c91d7810d978b89f
luizcgeraldo <luizclaudiogeraldo@gmail.com>
2023-03-23 16:41:44 -0300

    ajustes index.js

--- node/index.js
+++ node/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const mysql = require('mysql')
+
 const app = express()
 const port = 3000
+
 const config = {
   host: 'db',
   user: 'root',
@@ -8,20 +11,53 @@ const config = {
   database: 'nodedb'
 };
 
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
 
-const  sql = `select id, name from people` 
-connection.query(sql,
-    function(err, [rows]){
-        console.log(rows.name)
-        app.get('/',(req, res) => {
-            res.send('<h1>'+rows.name+'</h1>')    
-          })
+app.get('/', (req, res) => {
+  getPeople(res);
+});
+
+function verifPeople(conn){
+  
+  const  sql = `select id, name from people` ;
+
+  conn.query(sql, (error, results) => {
+    if (error) {
+      console.log(error);
+      return;
     }
- 
-);
-connection.end()
+
+  // Se não houver registros, insere um novo registro "Fullcycle"
+    if (results.length === 0) {
+      conn.query('INSERT INTO people (name) VALUES ("FullCycle Rocks!")', (error, results) => {
+        if (error) {
+          console.log(error);
+          return;
+        }
+        console.log('Novo registro inserido com sucesso.');
+      });
+    } else {
+      console.log('Já existem registros na tabela.');
+    }
+  });
+}
+
+async function getPeople(res){
+  const conn = mysql.createConnection(config);
+  const  sql = `select id, name from people` ;
+  
+  verifPeople(conn);
+  
+  setTimeout(() => {
+    console.log('esperando...');
+      //recupera registro e apresenta na tela 
+    conn.query(sql,
+      function(err, [rows]){
+          console.log(rows.name)
+          res.send('<h1>'+rows.name+'</h1>') 
+    });
+  }, 2000);
+  
+}
 
 app.listen(port, () => {
     console.log('Rodando na porta ' + port)
--- node/init_mysql/initdb.sql
+++ node/init_mysql/initdb.sql
@@ -3,14 +3,3 @@ CREATE TABLE IF NOT EXISTS `nodedb`.`people` (
   `id` INT AUTO_INCREMENT PRIMARY KEY,
   `name` VARCHAR(255) NOT NULL  
 );
-
-DELIMITER$$
-BEGIN
-   
-   IF NOT EXISTS (SELECT  NULL FROM people) THEN
-     INSERT INTO people (name) values ('FullCycles Rocks!');
-     COMMIT;
-   END IF;
-
-END$$
-DELIMITER;
